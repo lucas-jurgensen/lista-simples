ea4a14ca1bbbc75bb0b6e00e1e6fd33ef0dad23b
lucas-jurgensen <lucasjurgensenr@gmail.com>
2024-09-12 10:45:44 -0300

    alterações e adições

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const input = document.querySelector(".add-list");
 const list = document.querySelector("ul");
+const add_btn = document.querySelector(".add");
+const clear_btn = document.querySelector(".clear");
+const remove_btn = document.querySelector(".remove");
+
+list.addEventListener("click", (e) => {
+    let task_done = e.target;
+    task_done.classList.toggle("strike-through");
+
+    // console.log(task_done);
+});
 
 input.addEventListener("keyup", (e) => {
     if (e.key === "Enter") {
@@ -14,9 +24,6 @@ input.addEventListener("keyup", (e) => {
     }
 });
 
-const add_btn = document.querySelector(".add");
-const remove_btn = document.querySelector(".remove");
-
 add_btn.addEventListener("click", () => {
     if (input.value) {
         const new_li = document.createElement("li");
@@ -28,11 +35,19 @@ add_btn.addEventListener("click", () => {
     input.value = "";
 });
 
-remove_btn.addEventListener("click", () => {
+clear_btn.addEventListener("click", () => {
     if (list.firstChild) {
         while (list.firstChild) {
             list.removeChild(list.firstChild);
         }
+    } else {
+        alert("Sua lista já esta vazia");
+    }
+});
+
+remove_btn.addEventListener("click", () => {
+    if (list.lastChild) {
+        list.removeChild(list.lastChild);
     } else {
         alert("Sua lista já esta vazia");
     }
